07fd891583492893ba692c8ec6af945b0570cf68
Carlos Alonso Camiña <c.alonso@app-art.gr>
2025-08-05 15:38:47 +0300

    Create Landmarks project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Carlos Alonso Camiña on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
