82b4620f73d56fc6fe4120c6affd89498e547b54
Carlos Alonso Camiña <c.alonso@app-art.gr>
2025-08-19 09:06:11 +0300

    Complete Handling user input tutorial

--- Landmarks/Landmarks/Model/Landmark.swift
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -15,10 +15,13 @@ struct Landmark: Hashable, Codable, Identifiable {
     var park: String
     var state:String
     var description: String
+    var isFavorite: Bool
+    
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
+    
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
